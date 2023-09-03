6951050a3b917a683a467a16bbdd8c58976a50b0
espisepi <sepinaco@icloud.com>
2023-09-03 04:41:03 +0200

    yarn build to update server

--- build/types/features-custom/sketchbook-custom/world/WorldCustom.d.ts
+++ build/types/features-custom/sketchbook-custom/world/WorldCustom.d.ts
@@ -1,4 +1,5 @@
 import { World } from "../../../ts/sketchbook";
+import { VideoPoints } from "../prefabs/videopoints/VideoPoints";
 interface Control {
     desc: String;
     keys: Array<String>;
@@ -15,7 +16,9 @@ interface Position {
 }
 export declare class WorldCustom extends World {
     joystickState: JoystickState;
+    videoPoints: VideoPoints;
     constructor(worldScenePath?: any);
+    addObjectsInScene(): void;
     updateControls(controls: Array<Control>): void;
     private renderUIMobileButtons;
     private renderMovementButtons;
