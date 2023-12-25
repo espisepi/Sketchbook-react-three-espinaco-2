78554371d7f201b1a6dc98e9c567d50456512e15
espisepi <sepinaco@icloud.com>
2023-12-25 12:11:08 +0100

    relocate TODOs

--- src/features-custom/sketchbook-custom/sketchbookCustom.ts
+++ src/features-custom/sketchbook-custom/sketchbookCustom.ts
@@ -1,3 +1,20 @@
+export { WorldCustom as World } from "./world/WorldCustom";
 
+//TODOS Sepinaco:
+// - Add isMultiplayer boolean in "new Sketchbook.World(..., isMultiplayer: boolean) to create build with multiplayer or not multiplayer mode
+// - Improve Multiplayer mode
 
-export { WorldCustom as World } from './world/WorldCustom';
+// - Refactor functionality UI Mobile buttons using Sketchbook class like InputManager
+// - Refactor styles UI Mobile buttons
+// - Finish with UI Mobile buttons (add enter vehicle button and all buttons that is not still added)
+// - Add FirstPerson or 3rd person button
+
+// - Add new Realistic car with blender and include model gltf into this project
+// - Add new Realistic Plane with blender and include model gltf into this project
+// - Add new Realistic Helicopter with blender and include model gltf into this project
+
+// - Add new City with blender and include model gltf into this project
+// - Load cities in runtime to make the world more big
+
+// - Add many big Scenes with this technique: https://threejs.org/manual/#en/voxel-geometry
+// - With the before technique, add videoclip texture voxel trambolic scene
